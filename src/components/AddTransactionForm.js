798362c73214f681574d4ff50e9f815c206442b2
Derrick Nyasio <dnyasio2014@gmail.com>
2022-09-29 01:10:46 +0300

    Post functionality

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,21 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddTransactionForm() {
-  return (
-    <div className="ui segment">
-      <form className="ui form">
-        <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
-        </div>
-        <button className="ui button" type="submit">
-          Add Transaction
-        </button>
-      </form>
-    </div>
-  );
+    const [newTransaction, setNewTransaction] = useState({
+        date: "",
+        description: "",
+        category: "",
+        amount: "",
+    });
+
+    function handleChange(event) {
+        setNewTransaction({
+            ...newTransaction,
+            [event.target.name]: event.target.value
+        })
+    }
+    console.log(newTransaction);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        fetch("http://localhost:8001/transactions", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newTransaction),
+        })
+
+    }
+
+    return ( <
+        div className = "ui segment" >
+        <
+        form className = "ui form"
+        onSubmit = { handleSubmit } >
+        <
+        div className = "inline fields" >
+        <
+        input type = "date"
+        name = "date"
+        onChange = { handleChange }
+        value = { newTransaction.date }
+        /> <
+        input type = "text"
+        name = "description"
+        placeholder = "Description"
+        onChange = { handleChange }
+        value = { newTransaction.description }
+        /> <
+        input type = "text"
+        name = "category"
+        placeholder = "Category"
+        onChange = { handleChange }
+        value = { newTransaction.category }
+
+        /> <
+        input type = "number"
+        name = "amount"
+        placeholder = "Amount"
+        step = "0.01"
+        onChange = { handleChange }
+        value = { newTransaction.amount }
+        /> < /
+        div > <
+        button className = "ui button"
+        type = "submit" >
+        Add Transaction <
+        /button> < /
+        form > <
+        /div>
+    );
 }
 
 export default AddTransactionForm;
